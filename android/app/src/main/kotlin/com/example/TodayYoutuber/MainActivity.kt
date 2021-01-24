5b61054a6a6f3c347ffdbae8d2717045d2846d78
wonpyoJang <jangwonpyo0@gmail.com>
2021-01-25 00:03:21 +0900

    [added] 플러그인 receive_sharing_intent 1.4.2 - 설치(android side) 2

--- android/app/src/main/kotlin/com/example/TodayYoutuber/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/TodayYoutuber/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.TodayYoutuber
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
